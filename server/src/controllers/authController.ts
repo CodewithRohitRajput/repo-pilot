8c8a3fb4e13d34d1f7ee5f2a22a013674e5cbddf
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2026-07-06 04:16:48 +0530

    fixed res cookies

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -74,9 +74,9 @@ export const githubCallback = async(req: Request, res: Response) => {
 
     const token = generateToken(user._id.toString())
     res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
+        httpOnly: false,
+        secure: false,
+        sameSite: "lax",
         maxAge: 7 * 24* 60 * 60* 1000
     })
     return res.redirect("https://repo-pilot-five.vercel.app/dashboard")
